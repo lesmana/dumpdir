5cb286fb1871b8600abb1785bd1cb7cfa757cbe0
Lesmana Zimmer <lesmana@gmx.de>
2013-04-05 13:25:06 +0200

    reversedumpdir

---
+++ reversedumpdir.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+
+import os
+import sys
+
+# ------------------------------------------------------------------------------
+def main():
+  if len(sys.argv) == 2:
+    inputfilename = sys.argv[1]
+  else:
+    print 'need filename'
+    sys.exit(1)
+
+  with open(inputfilename) as inputfile:
+    for line in inputfile:
+      line = line.strip()
+      if not line:
+        continue
+      type, _, name = line.partition(' ')
+      if type == 'd':
+        currentfilename = None
+        os.mkdir(name)
+      elif type == 'f':
+        currentfilename = name
+        currentfile = open(currentfilename, 'w')
+        currentfile.close()
+      elif type == '>':
+        currentfile = open(currentfilename, 'a')
+        currentfile.write(name + '\n')
+        currentfile.close()
+      else:
+        raise Exception('unknown type: %s' % type)
+
+# ------------------------------------------------------------------------------
+if __name__ == '__main__':
+  main()
