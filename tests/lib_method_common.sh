30135e0b0b68334e2e3f3d7cec8826ae5eeeddae
Lesmana Zimmer <lesmana@gmx.de>
2013-04-08 23:25:57 +0200

    add catch fail functions

--- tests/lib_method_common.sh
+++ tests/lib_method_common.sh
@@ -1,4 +1,16 @@
 
+arrange_dirofdump() {
+  echo "you did not redefine arrange_dirofdump"
+  echo "you lost the game"
+  return 22
+}
+
+arrange_dumpofdir() {
+  echo "you did not redefine arrange_dumpofdir"
+  echo "you lost the game"
+  return 22
+}
+
 act_run_dumpdir() {
   workdir=$1
   outputdir=$2
