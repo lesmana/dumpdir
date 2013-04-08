fe74de935c442c1f189792423967c0b5ec813680
Lesmana Zimmer <lesmana@gmx.de>
2013-04-08 23:16:32 +0200

    extract functions for common code

--- tests/lib_act_loop.sh
+++ tests/lib_act_loop.sh
@@ -1,10 +1,18 @@
 
 . ../lib_act_common.sh
 
+arrange_in_stage() {
+  arrange_dirofdump
+}
+
+arrange_in_expected() {
+  arrange_dirofdump
+}
+
 arrange() {
   arrange_common
-  (cd stage; arrange_dirofdump)
-  (cd expected; arrange_dirofdump)
+  (cd stage; arrange_in_stage)
+  (cd expected; arrange_in_expected)
 }
 
 act() {
--- tests/lib_act_reverseloop.sh
+++ tests/lib_act_reverseloop.sh
@@ -1,10 +1,18 @@
 
 . ../lib_act_common.sh
 
+arrange_in_stage() {
+  arrange_dumpofdir
+}
+
+arrange_in_expected() {
+  arrange_dumpofdir
+}
+
 arrange() {
   arrange_common
-  (cd stage; arrange_dumpofdir)
-  (cd expected; arrange_dumpofdir)
+  (cd stage; arrange_in_stage)
+  (cd expected; arrange_in_expected)
 }
 
 act() {
