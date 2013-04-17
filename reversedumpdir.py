1f7dd10dc5f452e5681d5206839c49ee7088a7f4
Lesmana Zimmer <lesmana@gmx.de>
2013-04-17 21:27:20 +0200

    extract code to class main

--- reversedumpdir.py
+++ reversedumpdir.py
@@ -4,29 +4,39 @@ import os
 import sys
 
 # ------------------------------------------------------------------------------
-def main():
-  if len(sys.argv) == 2:
-    inputfilename = sys.argv[1]
-  else:
-    print 'need filename'
-    sys.exit(1)
+class Main(object):
 
-  with open(inputfilename) as inputfile:
-    for line in inputfile:
-      line = line.strip()
-      if not line:
-        continue
-      type, _, name = line.partition(' ')
-      if type == 'd':
-        currentfilename = None
-        os.mkdir(name)
-      elif type == 'f':
-        currentfilename = name
-        currentfile = open(currentfilename, 'w')
-        currentfile.close()
-      elif type == '>':
-        currentfile = open(currentfilename, 'a')
-        currentfile.write(name + '\n')
-        currentfile.close()
-      else:
-        raise Exception('unknown type: %s' % type)
+  def __init__(self):
+    pass
+
+  def run(self):
+    if len(sys.argv) == 2:
+      inputfilename = sys.argv[1]
+    else:
+      print 'need filename'
+      sys.exit(1)
+
+    with open(inputfilename) as inputfile:
+      for line in inputfile:
+        line = line.strip()
+        if not line:
+          continue
+        type, _, name = line.partition(' ')
+        if type == 'd':
+          currentfilename = None
+          os.mkdir(name)
+        elif type == 'f':
+          currentfilename = name
+          currentfile = open(currentfilename, 'w')
+          currentfile.close()
+        elif type == '>':
+          currentfile = open(currentfilename, 'a')
+          currentfile.write(name + '\n')
+          currentfile.close()
+        else:
+          raise Exception('unknown type: %s' % type)
+
+# ------------------------------------------------------------------------------
+def main():
+  mainrunner = Main()
+  mainrunner.run()
