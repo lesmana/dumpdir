34be4c948030db9c205276ca737ecf41b70b7a4f
Lesmana Zimmer <lesmana@gmx.de>
2013-04-24 18:43:22 +0200

    add test reversedumpdir

--- unittests/test_reversedumpdir.py
+++ unittests/test_reversedumpdir.py
@@ -1,7 +1,16 @@
 
 import unittest
 
+from reversedumpdir import ReverseDumpDir
+
 class TestReverseDumpDir(unittest.TestCase):
 
-  def test_todo(self):
-    assert False, 'todo'
+  def test_onedir(self):
+    report = []
+    class FakeOsMod(object):
+      def mkdir(self, name):
+        report.append(('mkdir', name))
+    revdd = ReverseDumpDir(FakeOsMod(), None)
+    revdd.reversedumpdir(['d somedir'])
+    self.assertEqual(report, [
+          ('mkdir', 'somedir')])
