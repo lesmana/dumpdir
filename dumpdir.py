5721a50c54f5d3a9390fb1459afd1f5f863dbf37
Lesmana Zimmer <lesmana@gmx.de>
2013-04-25 21:20:51 +0200

    extract class Main

--- dumpdir.py
+++ dumpdir.py
@@ -3,24 +3,34 @@
 import os
 
 # ------------------------------------------------------------------------------
+class Main(object):
+
+  def __init__(self):
+    pass
+
+  def run(self):
+    cwd = os.getcwd()
+    for (dirpath, dirnames, filenames) in os.walk(cwd):
+      dirnames.sort()
+      if dirpath != cwd:
+        relpath = os.path.relpath(dirpath)
+        print "d %s" % (relpath)
+      for filename in sorted(filenames):
+        dirpath_filename = os.path.join(dirpath, filename)
+        relpath_filename = os.path.relpath(dirpath_filename)
+        if os.path.islink(relpath_filename):
+          target = os.readlink(relpath_filename)
+          commonprefix = os.path.commonprefix([cwd, target])
+          if commonprefix != '/':
+            target = '(...)' + os.path.relpath(target)
+          print "l %s -> %s" % (relpath_filename, target)
+        else:
+          print "f %s" % (relpath_filename)
+          with open(relpath_filename) as fileobject:
+            for line in fileobject:
+              print "> %s" % (line.rstrip('\n'))
+
+# ------------------------------------------------------------------------------
 def main():
-  cwd = os.getcwd()
-  for (dirpath, dirnames, filenames) in os.walk(cwd):
-    dirnames.sort()
-    if dirpath != cwd:
-      relpath = os.path.relpath(dirpath)
-      print "d %s" % (relpath)
-    for filename in sorted(filenames):
-      dirpath_filename = os.path.join(dirpath, filename)
-      relpath_filename = os.path.relpath(dirpath_filename)
-      if os.path.islink(relpath_filename):
-        target = os.readlink(relpath_filename)
-        commonprefix = os.path.commonprefix([cwd, target])
-        if commonprefix != '/':
-          target = '(...)' + os.path.relpath(target)
-        print "l %s -> %s" % (relpath_filename, target)
-      else:
-        print "f %s" % (relpath_filename)
-        with open(relpath_filename) as fileobject:
-          for line in fileobject:
-            print "> %s" % (line.rstrip('\n'))
+  mainrunner = Main()
+  mainrunner.run()
