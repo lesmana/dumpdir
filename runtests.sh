ff3ef70c76cd681e6fb23b5994a031c973126dd5
Lesmana Zimmer <lesmana@gmx.de>
2017-11-10 11:49:19 +0100

    copy separately to prepare for m4

--- runtests.sh
+++ runtests.sh
@@ -5,6 +5,15 @@ PATH="$PWD:$PATH"
 cd tests
 rm -rf actualtests
 mkdir -p actualtests
-cp -a templates/* actualtests
+for datafile in templates/*_data.sh; do
+  for methodfile in templates/lib_method_*.sh; do
+    datapart=${datafile%_*}
+    methodpart=${methodfile##*_}
+    testfilename=${datapart}_${methodpart}
+    cp -a $testfilename actualtests
+  done
+  cp -a $datafile actualtests
+done
+cp -a templates/lib_* actualtests
 cd actualtests
 shut -r
