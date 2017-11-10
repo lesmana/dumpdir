cf11872fd6bd4193eb153528abe1e11112c07f3f
Lesmana Zimmer <lesmana@gmx.de>
2017-11-10 13:12:23 +0100

    explicit cd arrange and echo error exit

--- tests/templates/lib_runtest.sh
+++ tests/templates/lib_runtest.sh
@@ -3,8 +3,22 @@ arrange() {
   mkdir stage
   mkdir actual
   mkdir expected
-  (cd stage; arrange_in_stage) || exit
-  (cd expected; arrange_in_expected) || exit
+  (
+    cd stage
+    arrange_in_stage
+  ) || {
+    exitstatus=$?
+    echo "arange in stage failed"
+    exit $exitstatus
+  }
+  (
+    cd expected
+    arrange_in_expected
+  ) || {
+    exitstatus=$?
+    echo "arange in expected failed"
+    exit $exitstatus
+  }
 }
 
 assert() {
