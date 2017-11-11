bd7b9631111514d909fa2747df3613544438851a
Lesmana Zimmer <lesmana@gmx.de>
2017-11-11 11:25:06 +0100

    cosmetic: rename to targetfile

--- runtests.sh
+++ runtests.sh
@@ -9,9 +9,9 @@ for datafile in templates/data_*.sh; do
   for methodfile in templates/method_*.sh; do
     datapart=${datafile##*_}
     methodpart=${methodfile##*_}
-    targetfilename=actualtests/test_${datapart%.sh}_${methodpart}
-    m4 -I templates -DDATA=$datafile -DMETHOD=$methodfile test.sh > $targetfilename
-    chmod +x $targetfilename
+    targetfile=actualtests/test_${datapart%.sh}_${methodpart}
+    m4 -I templates -DDATA=$datafile -DMETHOD=$methodfile test.sh > $targetfile
+    chmod +x $targetfile
   done
 done
 cd actualtests
