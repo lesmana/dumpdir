0e02946e6d0fea2af0ca15526a3981c25bf6b75a
Lesmana Zimmer <lesmana@gmx.de>
2019-02-13 12:08:52 +0100

    merge lib_runtest.sh

--- tests/templates/test.sh
+++ tests/templates/test.sh
@@ -2,6 +2,37 @@
 
 include(DATA)
 include(METHOD)
-include(lib_runtest.sh)
+
+arrange() {
+  mkdir stage
+  mkdir actual
+  mkdir expected
+  (
+    cd stage
+    arrange_stage
+  ) || {
+    exitstatus=$?
+    echo "arange stage failed"
+    exit $exitstatus
+  }
+  (
+    cd expected
+    arrange_expected
+  ) || {
+    exitstatus=$?
+    echo "arange expected failed"
+    exit $exitstatus
+  }
+}
+
+assert() {
+  diff -r actual expected
+}
+
+runtest() {
+  arrange
+  act
+  assert
+}
 
 runtest
