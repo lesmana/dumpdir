5c85c32d9eccf5226198aafc919fe8911f9b8380
Lesmana Zimmer <lesmana@gmx.de>
2019-02-14 04:38:51 +0100

    rename to test_template

--- blackboxtests/generatetests.sh
+++ blackboxtests/generatetests.sh
@@ -5,7 +5,7 @@ mkdir -p generated
 for datafile in data_*; do
   for methodfile in method_*; do
     targetfile=generated/test_${datafile#data_}_${methodfile#method_}.sh
-    m4 -DDATA=$datafile -DMETHOD=$methodfile test > $targetfile
+    m4 -DDATA=$datafile -DMETHOD=$methodfile test_template > $targetfile
     chmod +x $targetfile
   done
 done
