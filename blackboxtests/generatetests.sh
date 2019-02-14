9d28432e21e30167709b9798d364ab0a85a6540b
Lesmana Zimmer <lesmana@gmx.de>
2019-02-14 04:25:09 +0100

    rename remove suffix sh

--- blackboxtests/generatetests.sh
+++ blackboxtests/generatetests.sh
@@ -2,12 +2,12 @@
 
 rm -rf generated
 mkdir -p generated
-for datafile in templates/data_*.sh; do
-  for methodfile in templates/method_*.sh; do
+for datafile in templates/data_*; do
+  for methodfile in templates/method_*; do
     datapart=${datafile##*_}
     methodpart=${methodfile##*_}
-    targetfile=generated/test_${datapart%.sh}_${methodpart}
-    m4 -I templates -DDATA=$datafile -DMETHOD=$methodfile test.sh > $targetfile
+    targetfile=generated/test_${datapart}_${methodpart}.sh
+    m4 -I templates -DDATA=$datafile -DMETHOD=$methodfile test > $targetfile
     chmod +x $targetfile
   done
 done
