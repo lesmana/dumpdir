646496606de114a885b49174ee8c96df8a2f9b75
Lesmana Zimmer <lesmana@gmx.de>
2019-02-14 02:03:14 +0100

    move test generation to test dir

---
+++ blackboxtests/runblackboxtests.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+rm -rf actualtests
+mkdir -p actualtests
+for datafile in templates/data_*.sh; do
+  for methodfile in templates/method_*.sh; do
+    datapart=${datafile##*_}
+    methodpart=${methodfile##*_}
+    targetfile=actualtests/test_${datapart%.sh}_${methodpart}
+    m4 -I templates -DDATA=$datafile -DMETHOD=$methodfile test.sh > $targetfile
+    chmod +x $targetfile
+  done
+done
+cd actualtests
+shut
--- runblackboxtests.sh
+++ runblackboxtests.sh
@@ -3,16 +3,4 @@
 PATH="$PWD:$PWD/blackboxtests/helpers:$PATH"
 
 cd blackboxtests
-rm -rf actualtests
-mkdir -p actualtests
-for datafile in templates/data_*.sh; do
-  for methodfile in templates/method_*.sh; do
-    datapart=${datafile##*_}
-    methodpart=${methodfile##*_}
-    targetfile=actualtests/test_${datapart%.sh}_${methodpart}
-    m4 -I templates -DDATA=$datafile -DMETHOD=$methodfile test.sh > $targetfile
-    chmod +x $targetfile
-  done
-done
-cd actualtests
-shut
+./runblackboxtests.sh
