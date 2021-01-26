a4c193da2eb317dd58834568ee1fe3a97c7ee4ad
Lesmana Zimmer <lesmana@gmx.de>
2021-01-26 10:21:55 +0100

    remove pedantic late import used for overly pedantic testing

--- dumpdir.py
+++ dumpdir.py
@@ -1,44 +1,38 @@
 #! /usr/bin/env python
 
+import os
+import sys
+
 # ------------------------------------------------------------------------------
 class DumpDir(object):
 
-  def __init__(self, osmod, openfunc, stdout):
-    self.osmod = osmod
-    self.openfunc = openfunc
-    self.stdout = stdout
-
   def dumpdir(self):
-    cwd = self.osmod.getcwd()
-    for (dirpath, dirnames, filenames) in self.osmod.walk(cwd):
+    cwd = os.getcwd()
+    for (dirpath, dirnames, filenames) in os.walk(cwd):
       dirnames.sort()
       if dirpath != cwd:
-        relpath = self.osmod.path.relpath(dirpath)
-        self.stdout.write('d %s\n' % (relpath))
+        relpath = os.path.relpath(dirpath)
+        sys.stdout.write('d %s\n' % (relpath))
       for filename in sorted(filenames):
-        dirpath_filename = self.osmod.path.join(dirpath, filename)
-        relpath_filename = self.osmod.path.relpath(dirpath_filename)
-        if self.osmod.path.islink(relpath_filename):
-          target = self.osmod.readlink(relpath_filename)
-          commonprefix = self.osmod.path.commonprefix([cwd, target])
+        dirpath_filename = os.path.join(dirpath, filename)
+        relpath_filename = os.path.relpath(dirpath_filename)
+        if os.path.islink(relpath_filename):
+          target = os.readlink(relpath_filename)
+          commonprefix = os.path.commonprefix([cwd, target])
           if commonprefix != '/':
-            target = '(...)' + self.osmod.path.relpath(target)
-          self.stdout.write('l %s -> %s\n' % (relpath_filename, target))
+            target = '(...)' + os.path.relpath(target)
+          sys.stdout.write('l %s -> %s\n' % (relpath_filename, target))
         else:
-          self.stdout.write('f %s\n' % (relpath_filename))
-          with self.openfunc(relpath_filename) as fileobject:
+          sys.stdout.write('f %s\n' % (relpath_filename))
+          with open(relpath_filename) as fileobject:
             for line in fileobject:
-              self.stdout.write('> %s\n' % (line.rstrip('\n')))
+              sys.stdout.write('> %s\n' % (line.rstrip('\n')))
 
   def runexcept(self, argv):
     self.dumpdir()
 
 # ------------------------------------------------------------------------------
 class ReverseDumpDir(object):
-
-  def __init__(self, osmod, openfunc):
-    self.osmod = osmod
-    self.openfunc = openfunc
 
   def reversedumpdir(self, inputfile):
     for line in inputfile:
@@ -48,13 +42,13 @@ class ReverseDumpDir(object):
       type, _, name = line.partition(' ')
       if type == 'd':
         currentfilename = None
-        self.osmod.mkdir(name)
+        os.mkdir(name)
       elif type == 'f':
         currentfilename = name
-        currentfile = self.openfunc(currentfilename, 'w')
+        currentfile = open(currentfilename, 'w')
         currentfile.close()
       elif type == '>':
-        currentfile = self.openfunc(currentfilename, 'a')
+        currentfile = open(currentfilename, 'a')
         currentfile.write(name + '\n')
         currentfile.close()
       else:
@@ -68,7 +62,7 @@ class ReverseDumpDir(object):
     return inputfilename
 
   def parsefileandcreatedirs(self, inputfilename):
-    with self.openfunc(inputfilename) as inputfile:
+    with open(inputfilename) as inputfile:
       self.reversedumpdir(inputfile)
 
   def runexcept(self, argv):
@@ -77,13 +71,11 @@ class ReverseDumpDir(object):
 
 # ------------------------------------------------------------------------------
 def main():
-  import os
-  import sys
   if '-r' in sys.argv:
     sys.argv.remove('-r')
-    dumpdirthing = ReverseDumpDir(os, open)
+    dumpdirthing = ReverseDumpDir()
   else:
-    dumpdirthing = DumpDir(os, open, sys.stdout)
+    dumpdirthing = DumpDir()
   try:
     exitstatus = dumpdirthing.runexcept(sys.argv)
   except Exception as error:
@@ -93,6 +85,5 @@ def main():
 
 # ------------------------------------------------------------------------------
 if __name__ == '__main__':
-  import sys
   exitstatus = main()
   sys.exit(exitstatus)
